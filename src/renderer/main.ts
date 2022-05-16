4c65c97a4f71f2c463b40a5e2f950e7650cb8829
tyin <tyin060@163.com>
2022-05-16 13:09:47 +0800

    main ElementPlus 优先级

--- src/renderer/main.ts
+++ src/renderer/main.ts
@@ -12,9 +12,9 @@ import {errorHandler} from "@renderer/error";
 import UpdatePicture from '@renderer/components/updatePicture/index.vue'
 import buttonPermission from "@renderer/instruct/buttonPermiss";
 const app = createApp(App);
-other.elSvg(app);
-app.component('update-picture', UpdatePicture)
 app.use(router).use(store, key).use(ElementPlus, {locale}).mount('#app');
 app.config.globalProperties.mittBus = mitt();
+app.component('update-picture', UpdatePicture)
 buttonPermission(app)
 errorHandler(app)
+other.elSvg(app);
